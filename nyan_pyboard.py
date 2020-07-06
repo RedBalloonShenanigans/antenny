173820aeab17fba2beb3db3f3679ba743a1a79d6
alexkhoma99 <ak4109@columbia.edu>
2020-07-06 18:33:17 -0400

    Adds docstrings to nyan* files

--- nyan_pyboard.py
+++ nyan_pyboard.py
@@ -1,8 +1,14 @@
 from mp.pyboard import Pyboard
 
 class NyanPyboard(Pyboard):
+    """Wrapper for Pyboard that adds eval_string_expr."""
 
     def eval_string_expr(self, expr_string):
+        """Return the result of a command string that will run on the ESP32.
+
+        Arguments:
+        expr_string -- A string containing the command to execute on the ESP32
+        """
         command = expr_string.encode('utf-8')
         ret = self.exec_("print(eval({}))".format(command))
         ret = ret.strip()
