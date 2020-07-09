da507395fd71a63cb3cf229bf1cea2e7ee0a1a12
alexkhoma99 <ak4109@columbia.edu>
2020-07-08 20:31:29 -0400

    Fixes 'import webrepl_setup' issue

--- wifi_config.py
+++ wifi_config.py
@@ -6,7 +6,10 @@ def query_wifi():
     wifi_dict['key'] = input('Your Password: ')
     with open('wifi_config.json', 'w') as f:
         json.dump(wifi_dict, f)
-
+    
+    webrepl_pass = input('Create WiFi console password: ')
+    with open('webrepl_cfg.py', 'w') as f:
+        f.write("PASS = '{}'\n".format(webrepl_pass))
 
 if __name__ == '__main__':
     query_wifi()
