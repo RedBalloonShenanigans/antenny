7e3be824bcab9da0a663c7b5f77e4f597d99f8f5
alexkhoma99 <ak4109@columbia.edu>
2020-08-07 14:23:12 -0400

    Changes TerminalPrinter to use static methods

--- nyansat/host/shell/antenny_client.py
+++ nyansat/host/shell/antenny_client.py
@@ -10,7 +10,7 @@ from time import sleep
 from dataclasses import dataclass
 from typing import List
 
-from nyansat.host.shell.terminal_printer import TerminalPrinter, print_warning
+from nyansat.host.shell.terminal_printer import TerminalPrinter
 from nyansat.host.shell.command_invoker import CommandInvoker
 from nyansat.host.shell.errors import *
 
@@ -127,7 +127,7 @@ class AntennyClient(object):
         self.guard_init()
         imu_enabled = self.invoker.config_get("use_imu")
         if imu_enabled == 'False':
-            TerminalPrinter().print_track_warning()
+            TerminalPrinter.print_track_warning()
         self.invoker.set_tracking(True)
         latitude = float(self.invoker.config_get("latitude"))
         longitude = float(self.invoker.config_get("longitude"))
@@ -171,7 +171,7 @@ class AntennyClient(object):
         data = (data['system'], data['gyroscope'], data['accelerometer'], data['magnetometer'])
         if not data:
             # TODO: should TerminalPrinter methods be static?
-            TerminalPrinter().print_error("Error connecting to BNO055.")
+            TerminalPrinter.print_error("Error connecting to BNO055.")
             return
 
         system_level, gyro_level, accel_level, magnet_level = data
@@ -180,7 +180,7 @@ class AntennyClient(object):
         accel_calibrated = accel_level > 0
         magnet_calibrated = magnet_level > 0
         components_calibrated = (system_calibrated, gyro_calibrated, accel_calibrated, magnet_calibrated)
-        TerminalPrinter()._display_initial_calibration_status(components_calibrated)
+        TerminalPrinter.display_initial_calibration_status(components_calibrated)
 
         waiting_dot_count = 4
         dot_counter = 0
@@ -188,7 +188,7 @@ class AntennyClient(object):
         while not (magnet_calibrated and accel_calibrated and gyro_calibrated):
             sleep(0.5)
             old_calibration_status = (system_calibrated, gyro_calibrated, accel_calibrated, magnet_calibrated)
-            system_calibrated, gyro_calibrated, accel_calibrated, magnet_calibrated = TerminalPrinter()._display_loop_calibration_status(
+            system_calibrated, gyro_calibrated, accel_calibrated, magnet_calibrated = TerminalPrinter.display_loop_calibration_status(
                 data,
                 old_calibration_status,
                 waiting_dot_count,
@@ -199,7 +199,7 @@ class AntennyClient(object):
             data = self.invoker.imu_calibration_status()
             data = (data['system'], data['gyroscope'], data['accelerometer'], data['magnetometer'])
             if not data:
-                TerminalPrinter().print_error("Error connecting to BNO055.")
+                TerminalPrinter.print_error("Error connecting to BNO055.")
                 return
 
             dot_counter = (dot_counter + 1) % waiting_dot_count
@@ -372,7 +372,7 @@ class AntennyClient(object):
             sda = int(input("SDA Pin#: "))
             scl = int(input("SCL Pin#: "))
         except ValueError:
-            TerminalPrinter().print_error("Invalid type for pin number. Try again using only decimal numbers")
+            TerminalPrinter.print_error("Invalid type for pin number. Try again using only decimal numbers")
             return
 
         bno_test_diagnostics = self.invoker.bno_diagnostics(sda, scl)
@@ -393,7 +393,7 @@ class AntennyClient(object):
             sda = int(input("SDA Pin#: "))
             scl = int(input("SCL Pin#: "))
         except ValueError:
-            TerminalPrinter().print_error("Invalid type for pin number. Try again using only decimal numbers")
+            TerminalPrinter.print_error("Invalid type for pin number. Try again using only decimal numbers")
             return
 
         pwm_test_diagnostics = self.invoker.pwm_diagnostics(sda, scl)
@@ -404,4 +404,3 @@ class AntennyClient(object):
         else:
             print("I2C address detected? True, addresses =", pwm_test_diagnostics.i2c_addresses)
         print("PWM connection established?", pwm_test_diagnostics.pca_object_created)
-
--- nyansat/host/shell/errors.py
+++ nyansat/host/shell/errors.py
@@ -1,5 +1,8 @@
 import logging
-from nyansat.host.shell.terminal_printer import print_error
+
+from abc import ABC
+
+from nyansat.host.shell.terminal_printer import TerminalPrinter
 from mp.pyboard import PyboardError
 
 
@@ -11,7 +14,7 @@ def exception_handler(func):
         try:
             func(*args, **kwargs)
         except AntennyException as e:
-            print_error(e.msg + '\n')
+            TerminalPrinter.print_error(e.msg + '\n')
             print_board_error(e)
 
     return wrapper
